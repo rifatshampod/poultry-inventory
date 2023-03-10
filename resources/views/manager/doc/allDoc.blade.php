3483038abf7591d53750700e1f0bdb984717461e
rifatshampod <shampodrifat@gmail.com>
2023-03-11 03:19:15 +0600

    manager doc page aligned with admin

--- resources/views/manager/doc/allDoc.blade.php
+++ resources/views/manager/doc/allDoc.blade.php
@@ -42,7 +42,7 @@
                                         <h4 class="card-title">All DOC ( Farm: <span class="text-primary">{{$farm->name}}</span>)</h4>
                                     </div>
                                     <div>
-                                        <button type="button" class="btn mb-1 btn-primary" data-toggle="modal" data-target="#addDoc">
+                                        <button type="button" class="btn mb-1 btn-primary" data-toggle="modal" data-target="#addDoc1">
                                             Add Doc
                                             <span class="btn-icon-right"><i class="fa fa-plus"></i></span>
                                         </button>
@@ -72,24 +72,22 @@
                                             <tr>
                                                 <td>{{$item->flock->name}}</td>
                                                 <td>{{$item->house->name}}</td>
-                                                <td>{{$item['date']}}</td>
+                                                <td>{{\Carbon\Carbon::parse($item['date'])->format('m/d/Y')}}</td>
                                                 <td>{{$item['sum_of_doc']}}</td>
                                                 <td>{{$item['hatchery']}}</td>
                                                 <td>{{$item['bird_in_case']}}</td>
                                                 <td>{{$item['vaccine']}}</td>
                                                 <td>{{$item['density']}}</td>
-                                                <td>{{$item['catching_start']}}</td>
-                                                <td>{{$item['catching_end']}}</td>
+                                                <td>{{\Carbon\Carbon::parse($item['catching_start'])->format('m/d/Y')}}</td>
+                                                <td>{{\Carbon\Carbon::parse($item['catching_end'])->format('m/d/Y')}}</td>
                                                 <td>
                                                     <div class="dropdown custom-dropdown float-right cursor">
                                                         <div data-toggle="dropdown">
                                                             <i class="fa fa-ellipsis-v display-7 display-7"></i>
                                                         </div>
                                                         <div class="dropdown-menu dropdown-menu-right">
-                                                            <a class="dropdown-item text-primary" href="#" data-toggle="modal" data-target="#addData">Add Data</a>
-                                                            <a class="dropdown-item text-warning" href="#">Sale</a>
-                                                            <a class="dropdown-item text-danger" href="#">View</a>
-                                                            <a class="dropdown-item text-success" href="#">Edit</a>
+
+                                                            <a class="dropdown-item text-success" id="{{$item['id']}}" onclick="openEditModal(this.id)">Edit</a>
                                                         </div>
                                                     </div>
                                                 </td>
@@ -105,19 +103,19 @@
                         </div>
                     </div>
 
+
                 </div>
             </div>
         </div>
 
         <!--------------------Modal Start----------------------->
         <!------------------Add Doc Modal Start----------------->
-        <div class="modal fade" id="addDoc">
+        <div class="modal fade" id="addDoc1">
             <div class="modal-dialog modal-dialog-centered" role="document">
                 <div class="modal-content">
                     <div class="modal-body">
                         <div class="mb-4">
                             <h5 class="modal-title">Add Doc (* marks are mandatory)</h5>
-
                         </div>
                         <form action="add-doc" method="POST">
                             @csrf
@@ -127,7 +125,6 @@
                                 <div class="col-lg-4">
                                     <div class="form-group">
                                         <label> Date <span class="text-danger">*</span></label>
-
                                         <input type="date" class="form-control input-default" name="date" placeholder="Input Start Date" required />
                                     </div>
                                 </div>
@@ -147,7 +144,6 @@
                                         <label>Farm <span class="text-danger">*</span></label>
 
                                         <select class="form-control input-default" name="farm_id" required>
-
                                             <option value="{{$farm->id}}" selected>{{$farm->name}}</option>
                                         </select>
 
@@ -158,7 +154,6 @@
                                         <label>House <span class="text-danger">*</span></label>
 
                                         <select class="form-control input-default" name="house_id" required>
-
                                             @foreach ($house as $item)
                                             <option value="{{$item['id']}}">{{$item['name']}}</option>
                                             @endforeach
@@ -170,7 +165,6 @@
                                         <label> Sum of Doc <span class="text-danger">*</span></label>
 
                                         <input type="number" class="form-control input-default" name="sum_of_doc" placeholder="Total Doc" required />
-
                                     </div>
                                 </div>
                                 <div class="col-lg-4">
@@ -253,6 +247,7 @@
         <!-------------------Add Doc Modal End ------------------>
 
 
+
         <!---------------------Modal End------------------------>
         <!--**********************************
             Content body end
@@ -270,6 +265,122 @@
         Main wrapper end
     ***********************************-->
 
+    <!-------edit-Modal------>
+    <div class="modal fade bs-example-modal-center" id="largeModal" tabindex="-1" role="dialog" aria-labelledby="largeModal" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered " style="min-width:60%;">
+            <div class="modal-content">
+                <div class="modal-body p-5">
+                    <form action="edit-doc-info" method="POST">
+                        @csrf
+                        <input type="hidden" name="chicken_id" id="edit_chicken_id">
+                        <div class="row">
+                            <div class="col-lg-4">
+                                <div class="form-group">
+                                    <label> Date</label>
+                                    <input type="date" id="edit_date" class="form-control input-default" name="date" placeholder="Input Start Date" />
+                                </div>
+                            </div>
+                            <div class="col-lg-4">
+                                <div class="form-group">
+                                    <label>House</label>
+                                    <select class="form-control input-default" name="house_id" id="edit_house">
+                                        @foreach ($house as $item)
+                                        <option value="{{$item['id']}}">{{$item['name']}}</option>
+                                        @endforeach
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="col-lg-4">
+                                <div class="form-group">
+                                    <label> Sum of Doc</label>
+                                    <input type="number" id="edit_sum" class="form-control input-default" name="sum_of_doc" placeholder="Total Doc" />
+                                </div>
+                            </div>
+                            <div class="col-lg-4">
+                                <div class="form-group">
+                                    <label> Hatchery</label>
+                                    <input type="text" id="edit_hatchery" class="form-control input-default" name="hatchery" placeholder="Hatchery Name" />
+                                </div>
+                            </div>
+                            <div class="col-lg-4">
+                                <div class="form-group">
+                                    <label> Bird In Case</label>
+                                    <input type="number" id="edit_birdincase" class="form-control input-default" name="bird_in_case" placeholder="Bird" />
+                                </div>
+                            </div>
+                            {{-- <div class="col-lg-12 mb-4">
+                                <div>
+                                    <label>Vaccination From Hacthery</label>
+                                </div>
+                                <div class="row">
+                                    <div class="col-lg-3">
+                                        <div class="form-check form-check-inline">
+                                            <label class="form-check-label">
+                                                <input type="checkbox" class="form-check-input" name="vaccine[]" value="ND + IB(Live)" />ND + IB(Live)</label>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-3">
+                                        <div class="form-check form-check-inline">
+                                            <label class="form-check-label">
+                                                <input type="checkbox" class="form-check-input" name="vaccine[]" value="NDS Yellow" />NDS Yellow</label>
+
+
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-3">
+                                        <div class="form-check form-check-inline">
+                                            <label class="form-check-label">
+                                                <input type="checkbox" class="form-check-input" name="vaccine[]" value="Transmune IBD" />Transmune IBD</label>
+
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-3">
+                                        <div class="form-check form-check-inline">
+                                            <label class="form-check-label">
+                                                <input type="checkbox" class="form-check-input" name="vaccine[]" value="Vectormune ND" />Vectormune ND</label>
+
+                                        </div>
+                                    </div>
+                                </div>
+                            </div> --}}
+                            <div class="col-lg-4">
+                                <div class="form-group">
+                                    <label> Density</label>
+                                    <input type="number" id="edit_density" class="form-control input-default" name="density" placeholder="Density" />
+                                </div>
+                            </div>
+                            <div class="col-lg-4">
+                                <div class="form-group">
+                                    <label> Catching Start Date</label>
+                                    <input type="date" id="edit_catchingStart" class="form-control input-default" name="catching_start" placeholder="" />
+                                </div>
+                            </div>
+                            <div class="col-lg-4">
+                                <div class="form-group">
+                                    <label> Catching End date</label>
+                                    <input type="date" id="edit_catchingEnd" class="form-control input-default" name="catching_end" placeholder="" />
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-12">
+                            <div class="d-flex justify-content-center">
+                                <div>
+                                    <button type="submit" class="btn btn-primary px-5 mx-1">
+                                        Update DOC Information
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <!-- /.modal-content -->
+        </div>
+        <!-- /.modal-dialog -->
+    </div>
+    <!---- End of edit modal ---->
+
+
     <!--**********************************
         Scripts
     ***********************************-->
@@ -281,5 +392,32 @@
     <script src="./plugins/tables/js/jquery.dataTables.min.js"></script>
     <script src="./plugins/tables/js/datatable/dataTables.bootstrap4.min.js"></script>
     <script src="./plugins/tables/js/datatable-init/datatable-basic.min.js"></script>
+
+    <script>
+        function openEditModal(clicked_id) {
+            $("#largeModal").modal("show");
+            //document.getElementById("getId").value = clicked_id;
+            $.ajax({
+                url: '/edit-doc' + clicked_id
+                , type: "GET"
+                , success: function(response) {
+                    console.log(response);
+                    $('#edit_date').val(response.chicken.date);
+                    $('#edit_farm').val(response.chicken.farm_id);
+                    $('#edit_house').val(response.chicken.house_id);
+                    $('#edit_sum').val(response.chicken.first_doc);
+                    $('#edit_hatchery').val(response.chicken.hatchery);
+                    $('#edit_birdincase').val(response.chicken.bird_in_case);
+                    $('#edit_density').val(response.chicken.density);
+                    $('#edit_catchingStart').val(response.chicken.catching_start);
+                    $('#edit_catchingEnd').val(response.chicken.catching_end);
+                    $('#edit_chicken_id').val(clicked_id);
+
+                }
+            });
+        }
+
+    </script>
+
 </body>
 </html>
